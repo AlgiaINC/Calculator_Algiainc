a045632f2c007b1967af78e4b759b9a2564b692c
Vladimir <raffik2014@yandex.by>
2024-02-16 23:30:18 +0300

    Save renaming changes create some objects and methods

--- src/main/java/com/algiainc/Main.java
+++ src/main/java/com/algiainc/Main.java
@@ -1,8 +1,10 @@
-package org.example;
+package com.algiainc;
 
 public class Main {
     public static void main(String[] args) {
         Person person1 = new Person("Олег", 28);
+        Person person2 = new Person("Ирина", 69);
         person1.sayInfo();
+        person2.sayInfo();
     }
 }
