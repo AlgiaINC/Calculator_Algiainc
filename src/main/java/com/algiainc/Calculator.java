33aee09308111084a239878bf61ed4fe7027abb5
Vladimir <raffik2014@yandex.by>
2024-02-17 21:06:10 +0300

    Debug the calculator end this feathere

--- src/main/java/com/algiainc/Calculator.java
+++ src/main/java/com/algiainc/Calculator.java
@@ -1,51 +1,44 @@
 package com.algiainc;
 
-public class Calculator {int number_01; int number_02;
-    public Calculator(int number_01, int number_02) {
-this.number_01 = number_01;
-this.number_02 = number_02;}
+public class Calculator {
+    public Calculator(int number_01, int number_02, double exponent) {
+        this.number_01 = number_01;
+        this.number_02 = number_02;
+        this.exponent = exponent;
+
+    }
+    int number_01;
+    int number_02;
+
     int summ;
     int substraction;
     int multiplex;
     float division;
     double building_in_degree;
-    int exponent;
+    double exponent;
     double square_root;
 
-    public int summ(int number_01, int number_02) {
+    public void summ() {
         summ = this.number_01 + this.number_02;
-        System.out.println("summ - The answer is " + summ);
-        return summ;
-    }
+        System.out.println("summ - The answer is " + summ);}
 
-    public int substraction(int number_01, int number_02) {
+    public void substraction() {
         substraction = this.number_01 - this.number_02;
-        System.out.println("substraction - The answer is " + substraction);
-        return substraction;
-    }
+        System.out.println("substraction - The answer is " + substraction);}
 
-    public int multiplex(int number_01, int number_02) {
+    public void multiplex() {
         multiplex = this.number_01 * this.number_02;
-        System.out.println("mult - The answer is " + multiplex);
+        System.out.println("mult - The answer is " + multiplex);}
 
-        return multiplex;
-    }
-
-    public float division(int number_01, int number_02) {
+    public void division() {
         division = this.number_01 / this.number_02;
-        System.out.println("division - The answer is " + division);
-        return division;
-    }
+        System.out.println("division - The answer is " + division);}
 
-    public double square_root(int number_01) {
+    public void square_root() {
         square_root = Math.sqrt(this.number_01);
-        System.out.println("square root - The answer is " + square_root);
-        return square_root;
-    }
+        System.out.println("square root - The answer is " + square_root);}
 
-    public double building_in_degree(int number_01, int exponent) {
+    public void building_in_degree() {
         building_in_degree = Math.pow(this.number_01, this.exponent);
-        System.out.println("building in degree - The answer is " + building_in_degree);
-        return building_in_degree;
-    }
+        System.out.println("building in degree - The answer is " + building_in_degree);}
 }
--- src/main/java/com/algiainc/Main.java
+++ src/main/java/com/algiainc/Main.java
@@ -4,15 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Person person1 = new Person("Олег", 28);
 
-        Calculator calculator1 = new Calculator(2,3);
+        Calculator calculator1 = new Calculator(10,5,2);
 
-        calculator1.summ(2,3);
-        calculator1.substraction(10,5);
+        calculator1.summ();
+        calculator1.substraction();
         calculator1.multiplex();
         calculator1.division();
 
-        calculator1.square_root(50);
-        calculator1.building_in_degree(6,2);
+        calculator1.square_root();
+        calculator1.building_in_degree();
 
     }
 
